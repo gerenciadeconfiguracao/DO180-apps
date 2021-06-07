1caa54b7790aa5a5ec76a4e62f10ef9f1489a502
Student User <student@workstation.lab.example.com>
2021-06-07 04:18:30 -0400

    files

--- copy.sh
+++ copy.sh
@@ -2,8 +2,10 @@
 sudo cp -Rf /usr/local/lib/* /home/student/DO180-apps/usr/local/lib/
 sudo cp -Rf /usr/local/sbin/* /home/student/DO180-apps/usr/local/sbin/
 sudo cp -Rf /usr/local/bin/* /home/student/DO180-apps/usr/local/bin/
+sudo cp -Rf /usr/local/bin/* /home/student/DO180-apps/usr/local/bin/
+sudo cp -Rf  /home/student/DO180/* /home/student/DO180-apps/home/student/DO180/
 
-sudo chown -R student:student /home/student/DO180-apps/usr/
+sudo chown -R student:student /home/student/DO180-apps/
 git add .
 git commit -m "files"
 git push origin master
